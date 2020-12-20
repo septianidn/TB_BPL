b0fe0565666c1b3f417a06b27141fe92b95b0167
ahmdfzn10 <73475538+ahmdfzn10@users.noreply.github.com>
2020-12-21 02:31:03 +0700

    Bagian fauzan
    Pembuatan kelas transaksi

---
+++ src/id/ac/unand/fti/si/TransaksiData.java
@@ -0,0 +1,33 @@
+package id.ac.unand.fti.si;
+
+public class TransaksiData {
+
+	String noresi;
+	String sku;
+	String username;
+	String date; 
+	String namaBarang; 
+	Integer jumlah;
+	Integer total;
+	static Integer stock;
+	
+	public TransaksiData() {
+		
+	}
+	
+	public TransaksiData(String noresi, String username, String date, String nama, Integer jumlah, Integer total) {
+		this.noresi=noresi;
+		this.username=username;
+		this.date=date;
+		this.namaBarang=nama;
+		this.jumlah=jumlah;
+		this.total=total;
+	}
+	
+	public TransaksiData(String noresi, String nama, Integer jumlah) {
+		this.noresi=noresi;
+		this.namaBarang=nama;
+		this.jumlah=jumlah;
+	}
+	
+}
